2526215d0ac2f59287b01fc25797190fa927f335
tomiris <kozhabekkyzy.t@northeastern.edu>
2022-04-06 14:16:04 -0400

    Changed tuits.json file. Added dislikes, comments and retuits

--- controllers/tuits/tuits.js
+++ controllers/tuits/tuits.js
@@ -3,12 +3,18 @@ export default [
     "_id": "123",
     "postedBy": {"username": "NASA"},
     "tuit": "In 2021, our @NASAPersevere Mars rover landed",
-    "likes": 345
+    "likes": 345,
+    "dislikes": 11,
+    "comments": 5,
+    "retuits": 20
   },
   {
     "_id": "234",
     "postedBy": { "username": "NASA" },
     "tuit": "Ingenuity helicopter takes flight.",
-    "likes": 346785
+    "likes": 346785,
+    "dislikes": 111,
+    "comments": 55,
+    "retuits": 27
   }
   ];

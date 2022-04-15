82241ea6790183cf4ff410e0668f1231dd0dcb64
tomiris <kozhabekkyzy.t@northeastern.edu>
2022-04-15 16:18:45 -0400

    Added logo-image in tuit schema

--- mongoose/tuits-schema.js
+++ mongoose/tuits-schema.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 const schema = mongoose.Schema({
   tuit: String,
   likes: Number,
+  logo_image: String,
   postedBy: {
     username: String }
 }, {collection: 'tuits'});
